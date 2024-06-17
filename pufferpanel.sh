211e863e36452326668616718cf93f710076e56b
vpsnodes <game.scripts.gg@gmail.com>
2024-06-17 12:05:18 +0400

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -3,7 +3,6 @@
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 sudo apt-get install pufferpanel
 sudo systemctl enable pufferpanel
-sudo pufferpanel user add --email foxytoux@gmail.com --name foxytoux --password Fox21200 --admin
-sudo systemctl enable --now pufferpanel
+sudo pufferpanel user add --email admin@zlx.gg --name admin --password 7 --admin
 
 # I uses https://docs.pufferpanel.com/en/2.x/installing.html to make this easy script, please look over there is there is any problem
